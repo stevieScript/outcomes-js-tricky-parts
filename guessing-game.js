4603fb4b2cd8c89578804be3b02f66996790ec80
Stevie <steviescripts52@gmail.com>
2023-07-23 15:49:15 -0700

    gitignore, install mock, guess game tests passing

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
-
+	const answer = Math.floor(Math.random() * 100);
+	let guesses = 0;
+	let isOver = false;
+	return function guess(num) {
+		if (isOver) return 'The game is over, you already won!';
+		guesses++;
+		if (num === answer) {
+			isOver = true;
+			const guess = guesses === 1 ? 'guess' : 'guesses';
+			return `You win! You found ${answer} in ${guesses} ${guess}.`;
+		}
+		if (num < answer) return `${num} is too low!`;
+		if (num > answer) return `${num} is too high!`;
+	};
 }
 
-module.exports = { guessingGame };
+module.exports = {guessingGame};
+
